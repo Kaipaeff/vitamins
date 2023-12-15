a34a82a7ce76749e54f2c56ce1101c10d1023b72
Aslanbek <kaipaeff@gmail.com>
2023-12-15 10:20:13 +0300

    pinia installed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { createPinia } from 'pinia';
 import { createApp } from 'vue'
 import './scss/settings/normalize.scss';
 import './scss/style.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia()).mount('#app')
